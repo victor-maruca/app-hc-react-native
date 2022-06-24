3ce596d79bb425c89d1bea1ba63f4a24e6c42eef
João Maruca <victor-maruca03@hotmail.com>
2022-06-23 22:45:27 -0300

    feat: Create FAQ screen with list nd searchbar

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#673AB7',
+    secondary: '#009688',
+    tertiary: '#3F51B5',
+    quaternary: '#9C27B0'
+  },
+};
 
 export default function Main() {
     return (
       <NavigationContainer>
-        <PaperProvider>
+        <PaperProvider theme={theme}>
           <App />
         </PaperProvider>
       </NavigationContainer>
